b5f5e049523949f6b76a13e11ab6e9038a0f253f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-12-27 22:34:11 +0100

    testing rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,22 @@
+import { it, expect, describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Greet from '../../src/components/Greet';
+import '@testing-library/jest-dom/vitest';
+
+describe('Greet', () => {
+  it('should render Hello with the name when name is provided', () => {
+    render(<Greet name='Kemi' />);
+
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/kemi/i);
+  });
+
+  it('should render Login button when name is not provided', () => {
+    render(<Greet />);
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
