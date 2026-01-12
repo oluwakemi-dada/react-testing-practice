a07454aee7e17c718b2816fdcc4d7e013a46780a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-01-12 11:31:39 +0100

    test ExpandableText

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,9 @@
-import Onboarding from '../components/Onboarding';
-import TermsAndConditions from '../components/TermsAndConditions';
+import ExpandableText from '../components/ExpandableText';
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text='  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor porro quam ullam recusandae laborum tempora, velit aspernatur tenetur placeat. Inventore non, doloremque aliquid ea porro ipsa alias ab sed suscipit eius, velit ipsum nihil beatae? Reiciendis illum ab praesentium aut porro, sed tempore rem laborum excepturi quidem. Blanditiis nulla perferendis reiciendis libero exercitationem, iusto velit delectus voluptates, temporibus, architecto nam quo vel ad accusamus quidem repudiandae illum cumque eius aperiam. Ex doloribus, molestiae vel iusto ducimus maxime ratione. Neque soluta illum labore ad pariatur, voluptatibus aut alias quos nam exercitationem perspiciatis nostrum eos laborum harum, omnis expedita dicta quasi ratione?' />
+  );
 };
 
 export default PlaygroundPage;
