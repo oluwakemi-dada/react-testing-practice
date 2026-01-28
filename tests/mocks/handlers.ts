c5da4706c56903c51810723222248f290f67d65e
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-01-28 18:18:15 +0100

    test data fetching

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,19 +1,23 @@
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse } from "msw";
+import { products } from "./data";
 
 export const handlers = [
-  http.get('/categories', () => {
+  http.get("/categories", () => {
     return HttpResponse.json([
-      { id: 1, name: 'Electronics' },
-      { id: 2, name: 'Beauty' },
-      { id: 3, name: 'Gardening' },
+      { id: 1, name: "Electronics" },
+      { id: 2, name: "Beauty" },
+      { id: 3, name: "Gardening" },
     ]);
   }),
 
-  http.get('/products', () => {
-    return HttpResponse.json([
-      { id: 1, name: 'Product 1' },
-      { id: 2, name: 'Product 2' },
-      { id: 3, name: 'Product 3' },
-    ]);
+  http.get("/products", () => {
+    return HttpResponse.json(products);
+  }),
+
+  http.get("/products/:id", ({ params }) => {
+    const { id } = params;
+    const product = products.find((p) => p.id === Number(id));
+    if (!product) return new HttpResponse(null, { status: 404 });
+    return HttpResponse.json(product);
   }),
 ];
