fceb71674da3e3b7ac7d4dde54e4e2a5df3a701d
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-18 11:19:23 +0100

    extract category select

--- src/pages/BrowseProductsPage.tsx
+++ src/pages/BrowseProductsPage.tsx
@@ -1,18 +1,14 @@
-import { Select, Table } from '@radix-ui/themes';
-import axios, { AxiosError } from 'axios';
-import { useEffect, useState } from 'react';
+import { Table } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import QuantitySelector from '../components/QuantitySelector';
-import { Category, Product } from '../entities';
-import { useQuery } from '@tanstack/react-query';
+import { Product } from '../entities';
+import CategorySelect from '../components/CategorySelect';
 
 function BrowseProducts() {
-  const categoriesQuery = useQuery({
-    queryKey: ['categories'],
-    queryFn: () => axios.get<Category[]>('/categories').then((res) => res.data),
-  });
-
   const productsQuery = useQuery({
     queryKey: ['products'],
     queryFn: () => axios.get<Product[]>('/products').then((res) => res.data),
@@ -24,40 +20,6 @@ function BrowseProducts() {
 
   if (productsQuery.error)
     return <div>Error: {productsQuery.error.message}</div>;
-
-  const renderCategories = () => {
-    const { isLoading, error, data: categories } = categoriesQuery;
-
-    if (isLoading)
-      return (
-        <div role='progressbar' aria-label='Loading categories'>
-          <Skeleton />
-        </div>
-      );
-
-    if (error) return null;
-
-    return (
-      <Select.Root
-        onValueChange={(categoryId) =>
-          setSelectedCategoryId(parseInt(categoryId))
-        }
-      >
-        <Select.Trigger placeholder='Filter by Category' />
-        <Select.Content>
-          <Select.Group>
-            <Select.Label>Category</Select.Label>
-            <Select.Item value='all'>All</Select.Item>
-            {categories!.map((category) => (
-              <Select.Item key={category.id} value={category.id.toString()}>
-                {category.name}
-              </Select.Item>
-            ))}
-          </Select.Group>
-        </Select.Content>
-      </Select.Root>
-    );
-  };
 
   const renderProducts = () => {
     const skeletons = [1, 2, 3, 4, 5];
@@ -114,7 +76,11 @@ function BrowseProducts() {
   return (
     <div>
       <h1>Products</h1>
-      <div className='max-w-xs'>{renderCategories()}</div>
+      <div className='max-w-xs'>
+        <CategorySelect
+          onChange={(categoryId) => setSelectedCategoryId(categoryId)}
+        />
+      </div>
       {renderProducts()}
     </div>
   );
