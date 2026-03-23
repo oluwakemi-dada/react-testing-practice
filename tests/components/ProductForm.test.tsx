ad1a24eddb413cbf7d57ea1b34279affbbbf3f2a
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-23 11:22:04 +0100

    resolve act warning

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -60,6 +60,7 @@ describe('ProductForm', () => {
           if (product.price !== undefined)
             await user.type(priceInput, product.price.toString());
 
+          await user.tab();
           await user.click(categoryInput);
           const options = screen.getAllByRole('option');
           await user.click(options[0]);
