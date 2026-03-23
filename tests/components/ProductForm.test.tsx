85c5de6d346b38f4c412a24b52bb3c897e59aa8f
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-03-23 11:36:24 +0100

    test form submission

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -6,6 +6,7 @@ import ProductForm from '../../src/components/ProductForm';
 import AllProviders from '../AllProviders';
 import { Category, createCategory, deleteCategory, Product } from '../mocks/db';
 import userEvent from '@testing-library/user-event';
+import { Toaster } from 'react-hot-toast';
 
 describe('ProductForm', () => {
   let category: Category;
@@ -19,11 +20,21 @@ describe('ProductForm', () => {
   });
 
   const renderComponent = (product?: Product) => {
-    render(<ProductForm product={product} onSubmit={vi.fn()} />, {
-      wrapper: AllProviders,
-    });
+    const onSubmit = vi.fn();
+
+    render(
+      <>
+        <ProductForm product={product} onSubmit={onSubmit} />,
+        <Toaster />
+      </>,
+      {
+        wrapper: AllProviders,
+      },
+    );
 
     return {
+      onSubmit,
+
       expectErrorToBeInTheDocument: (errorMessage: RegExp) => {
         const error = screen.getByRole('alert');
         expect(error).toBeInTheDocument();
@@ -48,7 +59,7 @@ describe('ProductForm', () => {
           id: 1,
           name: 'a',
           price: 1,
-          categoryId: 1,
+          categoryId: category.id,
         };
 
         const fill = async (product: FormData) => {
@@ -172,4 +183,27 @@ describe('ProductForm', () => {
       expectErrorToBeInTheDocument(errorMessage);
     },
   );
+
+  it('should call onSubmit with the correct data', async () => {
+    const { waitForFormToLoad, onSubmit } = renderComponent();
+
+    const form = await waitForFormToLoad();
+    await form.fill(form.validData);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars
+    const { id, ...formData } = form.validData;
+    expect(onSubmit).toHaveBeenCalledWith(formData);
+  });
+
+  it('should display a toast if submission fails', async () => {
+    const { waitForFormToLoad, onSubmit } = renderComponent();
+    onSubmit.mockRejectedValue({});
+
+    const form = await waitForFormToLoad();
+    await form.fill(form.validData);
+
+    const toast = await screen.findByRole('status');
+    expect(toast).toBeInTheDocument();
+    expect(toast).toHaveTextContent(/error/i);
+  });
 });
